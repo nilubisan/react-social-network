86bf78f7893a0470ee9dbc1d902feeb56abcd1ba
nilubisan <r.nas9329@gmail.com>
2022-05-25 22:41:14 +0600

    feat: add redux extension support for chromium

--- src/redux/redux.ts
+++ src/redux/redux.ts
@@ -1,4 +1,4 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import AppReducer from './reducers/app-reducer';
 import AuthReducer from './reducers/auth-reducer';
@@ -15,5 +15,6 @@ const reducers = combineReducers({
   authData: AuthReducer,
   appData: AppReducer,
 });
-const store = createStore(reducers, applyMiddleware(thunk));
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 export default store;
